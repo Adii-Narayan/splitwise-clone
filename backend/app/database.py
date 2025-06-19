a7220e1d0147ea7b6f19f06c48e8650ae444caf8
Adii-Narayan <112889026+Adii-Narayan@users.noreply.github.com>
2025-06-19 12:44:22 +0530

    Add Render-ready .env config and backend updates

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/splitwise"
+# Load environment variables from .env
+load_dotenv()
 
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
